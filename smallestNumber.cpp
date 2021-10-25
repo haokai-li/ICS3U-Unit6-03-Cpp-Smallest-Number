a01d78e1489139cf7bb914185e5d5a851d2089e1
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-10-25 16:32:40 +0000

    smalleat number

---
+++ smallestNumber.cpp
@@ -0,0 +1,61 @@
+// Copyright (c) 2021 haokai All rights reserved
+//
+// Created by: Haokai
+// Created on: Sept 2021
+// This Program calculate smallest number
+
+#include <iostream>
+#include <random>
+#include <array>
+
+template<size_t N>
+int calculatedSmallestNumber(std::array<int, N> calculateMyNumbers) {
+    // This function calculate smallest number
+    int totalNumber = 100;
+    int times = 0;
+
+    // process
+    for (int loopNumberSecond : calculateMyNumbers) {
+        times += 1;
+        // output
+        std::cout << "The random number " << times << " is "
+        << loopNumberSecond << std::endl;
+
+        if (loopNumberSecond < totalNumber) {
+            totalNumber = loopNumberSecond;
+        }
+    }
+
+    return totalNumber;
+}
+
+int main() {
+    // This Program calculate average
+    std::array<int, 10> myNumbers;
+
+    int loopNumberFirst;
+
+    int calculateNumber;
+    int smallestNumber;
+
+    // process
+    for (loopNumberFirst = 0; loopNumberFirst < 10; loopNumberFirst++) {
+        std::random_device rseed;
+        std::mt19937 rgen(rseed());
+        std::uniform_int_distribution<int> idist(0, 100);
+        calculateNumber = idist(rgen);
+        myNumbers[loopNumberFirst] = calculateNumber;
+    }
+
+    // output
+    std::cout << "Here is a list of random numbers:" << std::endl;
+    std::cout << "" << std::endl;
+
+    // call functions
+    smallestNumber = calculatedSmallestNumber(myNumbers);
+
+    std::cout << "" << std::endl;
+    std::cout << "The smallest number is: " << smallestNumber << std::endl;
+
+    std::cout << "\nDone" << std::endl;
+}
